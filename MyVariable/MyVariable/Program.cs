650dc45df32085f34451f7bf516a34c1739bb740
Ramylinux <ramy_work@yahoo.com>
2017-09-13 14:25:32 +0300

    Initial commit

---
+++ MyVariable/MyVariable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int _myValue=7;
+            Console.WriteLine("{0}", _myValue);
+            Console.ReadKey();
+
+        }
+    }
+}
