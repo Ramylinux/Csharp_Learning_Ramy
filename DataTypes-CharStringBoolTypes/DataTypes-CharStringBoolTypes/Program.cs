7e4dfabe4a35a25ea4df7e8e705bc3e13ebd4739
Ramylinux <ramy_work@yahoo.com>
2017-09-19 14:17:28 +0300

    With Challenge2

--- DataTypes-CharStringBoolTypes/DataTypes-CharStringBoolTypes/Program.cs
+++ DataTypes-CharStringBoolTypes/DataTypes-CharStringBoolTypes/Program.cs
@@ -58,7 +58,32 @@ namespace DataTypes_CharStringBoolTypes
             Console.WriteLine("My Boolean Value is = {0}\n", myBool);
 
 
+            // Question : Printe the variable in this Form " This is My Character "A" Value "??" and this is My Character "a" value "??" 
+
+            char Char_A = 'A';
+            char char_a = 'a';
+            int MyVlue_A = Convert.ToChar(Char_A);
+            int myVlue_a = Convert.ToChar(char_a);
+
+            Console.WriteLine("\n This is My Character \"{0}\" Value = \"{1}\" and this is My Character \"{2}\" value = \"{3}\"\n" , Char_A ,MyVlue_A, char_a,myVlue_a);
+
+            // Or another Soulation for Question
+            int myVar1 = 'A', myVar2 = 'a';
+            string myString1 = "This is my character A value ", myString2 = " This is my character a value ";
+            Console.WriteLine("\"" + myString1 + myVar1 + myString2 + myVar2 + "\"");
+            
+            // OR
+
+            int myletter1 = 'R', myletter2 = 'r';
+
+            Console.WriteLine("\nMy Letter \"R\" is = {0} and My Small letter is \"r \" is = {1}\n", myletter1,myletter2);
+            Console.WriteLine("Myletter R =" + "\""  +myletter1+ "\" " + "MyLetter r =" + "\""  +myletter2 + "\"");
+
             Console.ReadKey();
+
+
+
+           
 
         }
     }
