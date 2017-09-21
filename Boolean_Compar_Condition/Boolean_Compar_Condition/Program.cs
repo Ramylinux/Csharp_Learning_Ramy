7a517f6382fee005f644591d9d30265ac41143f2
Ramylinux <ramy_work@yahoo.com>
2017-09-21 09:38:25 +0300

    Boolean_Compar_Condition

--- Boolean_Compar_Condition/Boolean_Compar_Condition/Program.cs
+++ Boolean_Compar_Condition/Boolean_Compar_Condition/Program.cs
@@ -17,6 +17,17 @@ namespace Boolean_Compar_Condition
             myResult_or = (myVar1 < 10) || (myVar2 <= 11);
 
             Console.WriteLine($" Notes: \n myVar1 = 9 \n myVar2 = 11 \n\n Result && is = {myResult}\n\n Result || is {myResult_or}\n\n");
+
+            /*==========================================================*/
+
+            //int myVar1 = 9, myVar2 = 11;
+            bool myConditionResult;
+
+            myConditionResult = ((myVar1 <= 9) && (myVar2 <= 11));
+            Console.WriteLine("The Result of my condition is {0}", myConditionResult);
+            myConditionResult = ((myVar1 <= 9) || (myVar2 <= 11));
+            Console.WriteLine("The Result of my condition is {0}\n\n", myConditionResult);
+
             Console.ReadKey();
 
 
