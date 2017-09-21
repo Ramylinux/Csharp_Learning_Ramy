5b19114b5a1e2042d74f32fd8c1fd2cdc6cd61e8
Ramylinux <ramy_work@yahoo.com>
2017-09-21 16:09:24 +0300

    Tutorial_4_Ternary_Condition

---
+++ Tutorial_4_Ternary_Condition/Tutorial_4_Ternary_Condition/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_4_Ternary_Condition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Q1
+
+            int myVar1 = 2;
+            string myResult;
+            myResult=(myVar1>=9)?"Good Work":"bad work";
+            Console.WriteLine($"this is {myResult}\n\n");
+
+            //Q2
+
+            string myName = "Ramy AbuDhair",result;
+
+            result = myName.Equals("Ramy AbuDhair") ? "Yes" : "No";
+            Console.WriteLine($"\n\n My variable is {result}\n\n");
+
+
+        }
+    }
+}
