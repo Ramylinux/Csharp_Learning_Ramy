585bc0a5b49a076fb5e109a3e1c43250b2b9f42b
Ramylinux <ramy_work@yahoo.com>
2017-09-21 14:53:38 +0300

    Switch_cases

---
+++ Tutorial_3_Switch_statement/Tutorial_3_Switch_statement/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_3_Switch_statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string whatHesay;
+            whatHesay = "Hello";
+            switch (whatHesay)
+                {
+               
+                case "Baye":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Baye\n\n\n");
+
+                    break;
+                case "Welcome":
+                    Console.WriteLine("Welcome\n\n");
+                    break;
+                case "Hello":
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Hello\n\n");
+                    break;
+                default:
+                   Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("No one\n\n");
+                    break;
+                   
+            }
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
