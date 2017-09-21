559774f0ae4ce555e80740e3292bcb72c9df2c7f
Ramylinux <ramy_work@yahoo.com>
2017-09-21 09:34:09 +0300

    Boolean_Comparsion Boolean_Condition

---
+++ Boolean_Compar_Condition/Boolean_Compar_Condition/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_Compar_Condition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar1 = 9, myVar2 = 11;
+            bool myResult, myResult_or;
+
+            myResult = (myVar1 < 10) && (myVar2 <= 11);
+            myResult_or = (myVar1 < 10) || (myVar2 <= 11);
+
+            Console.WriteLine($" Notes: \n myVar1 = 9 \n myVar2 = 11 \n\n Result && is = {myResult}\n\n Result || is {myResult_or}\n\n");
+            Console.ReadKey();
+
+
+        }
+    }
+}
