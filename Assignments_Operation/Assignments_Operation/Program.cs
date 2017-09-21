d72e9d9915c7113ba4d5bd21eae6572a6031b433
Ramylinux <ramy_work@yahoo.com>
2017-09-21 09:21:33 +0300

    increments

---
+++ Assignments_Operation/Assignments_Operation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments_Operation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  myVar1 = 7, myVar3 = 7,myVar2 = 2;
+
+            myVar1 /= ++myVar2;
+
+            Console.WriteLine($"\n\n myVar1={myVar3} / = ++myVar2=++{myVar2} is result var1={myVar1} Var2={myVar2}\n");
+
+
+
+        //OR 
+
+            Console.WriteLine("The result of the expression above is {0}", myVar1);
+
+        
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
