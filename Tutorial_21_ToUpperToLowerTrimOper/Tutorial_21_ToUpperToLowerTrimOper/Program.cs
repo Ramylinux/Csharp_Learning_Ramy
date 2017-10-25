68e593d754036405ab8d2357960f85bb8311e378
Ramylinux <ramy_work@yahoo.com>
2017-10-25 15:43:22 +0300

    Tutorial_21_ToUpperToLowerTrimOper

---
+++ Tutorial_21_ToUpperToLowerTrimOper/Tutorial_21_ToUpperToLowerTrimOper/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_21_ToUpperToLowerTrimOper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tutorial From Course
+
+            string myString, myString2, readString, trimString;
+
+            Console.WriteLine($"Please Enter Your String : ");
+
+            
+                 myString = Console.ReadLine();
+                 myString2 = myString.ToUpper();
+       
+                Console.WriteLine($"{myString2}");
+                 myString2 = myString.ToLower();
+
+
+                 myString2 = myString.Trim(); // Trim from start and End
+
+                 Console.WriteLine($"{myString2}\n\n");
+
+        
+            Console.ReadKey();
+
+            char[] mytrimChar = {'i','s','r',' '}; //Trim From Start and End
+
+            Console.WriteLine($"Please Enter Your Char : ");
+            readString = Console.ReadLine();
+            readString = readString.ToLower();
+            trimString = readString.Trim(mytrimChar);
+
+            //  Console.WriteLine($"{readString}" +"\n\n\n\n" + $"{trimString}");
+            //  Console.WriteLine($"{trimString}");
+
+            Console.WriteLine($"{trimString}");
+
+
+
+
+        }// Close ==> static void Main(string[] args)
+        //-----------------------------
+
+
+    }
+}
