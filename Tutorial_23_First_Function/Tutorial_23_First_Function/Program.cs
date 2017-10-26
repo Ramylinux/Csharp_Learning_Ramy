d51c32b5aafe2e1084bbcd03a985d4f3995e60d6
Ramylinux <ramy_work@yahoo.com>
2017-10-26 14:34:34 +0300

    Tutorial_23_First_Function

---
+++ Tutorial_23_First_Function/Tutorial_23_First_Function/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_23_First_Function
+{
+    class Program
+    {
+        static void print_Name() //You Can the First Character is Capital letter 
+        {
+            Console.WriteLine("My Name Ramy Abu-Dhair");
+        }
+        static void Main(string[] args)
+        {
+            print_Name();
+            Console.ReadKey();
+        }
+    }
+}
