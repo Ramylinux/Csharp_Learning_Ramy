3ce96ef096b365e07b8b8da2fa7b36a6ff688512
Ramylinux <ramy_work@yahoo.com>
2017-10-30 16:21:04 +0300

    Return Value

--- Tutorial_24_F_ParameterVSArguments/Tutorial_24_F_ParameterVSArguments/Program.cs
+++ Tutorial_24_F_ParameterVSArguments/Tutorial_24_F_ParameterVSArguments/Program.cs
@@ -19,24 +19,47 @@ namespace Tutorial_24_F_ParameterVSArguments
             int tmp = Number1 + Number2;
             return tmp;
         }
+        static string any_Names(string fName,string lName)
+        {
+                   
+            string fulName = ($"{fName} {lName}");
+
+            return fulName;
+        }
+
 
 
         static void Main(string[] args)
         {
-            int myVar1 = 3, myVar2 = 5,tmp;
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-            addNumbers(myVar1,myVar2);
+            int myVar1 = 3, myVar2 = 5, tmp;
+            //++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+            addNumbers(myVar1, myVar2); // call Function 1
             Console.ReadKey();
             // +++++++++++++++++++++++++++++++++++++++++++++++++++++++
-             tmp=addNumbers2(myVar1, myVar2); // You can change name virable "tmp" to another name (No problem)
+            // call Function 2
+            tmp = addNumbers2(myVar1, myVar2); // You can change name virable "tmp" to another name (No problem)
             Console.WriteLine($"\n\nTotal function Add NUmber 2 = {tmp}\n\n");
 
             // OR // No Need Vairable name tmp: you can call function direct
             // addNumbers2(myVar1, myVar2);
             // Console.WriteLine($"\n\nTotal function Add NUmber 2 = {addNumbers2(myVar1, myVar2)}\n\n");
+            string fullName1, fName, lName;// OR fName="Ramy ", lName=" Abu-Dhair";
 
-        
+            Console.WriteLine(" Enter First Name \n");
+
+            fName = Console.ReadLine();
+
+            Console.WriteLine(" Enter Last Name \n");
+
+            lName = Console.ReadLine();
+
+            fullName1 = any_Names(fName, lName);
+            
+            Console.WriteLine($"\n\n{fullName1}\n");
+
+            Console.WriteLine("Thank you \n");
             Console.ReadKey();
+
         }//Colse ==> static viod Main(string[] args)
 
 
---
+++ Tutorial_25_Return_Values/Tutorial_25_Return_Values/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_25_Return_Values
+{
+    class Program
+    {
+        //Example form Course
+        // Mininmum Vales
+        static int MinValue(int[] myArray)
+        {
+            int minval1 = myArray[0];
+            for(int i=1; i < myArray.Length; i++)
+            {
+                if (myArray[i]<minval1)
+                    minval1 = myArray[i];
+            }
+
+
+            return minval1;
+        }
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //Question 26
+        //
+        static double AvarageRate(int [] arrayAvarage)
+        {
+            double myAvarage = arrayAvarage[0];
+            double total;
+            for (int i =1;i<arrayAvarage.Length;i++)
+            {
+                 myAvarage = myAvarage + arrayAvarage[i];
+                
+            }
+           // Console.WriteLine($"my Avarage from this Function is {myAvarage}\n");
+            total = myAvarage / 7;
+
+            return total;
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+
+       //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+       //Example form Course
+            int[] myarray = { 10, 22, 34, 5, 6, 78, 2, 11 };
+          int minVal1= MinValue(myarray);
+          Console.WriteLine($"The Minimum Value in my Array is : {minVal1}");
+          Console.ReadKey();
+
+            //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+            //Question 26
+            //Define a function to find the avarage
+            //of a student that has 7 grades and print the
+            //result in the void main
+
+            Console.WriteLine(@"//Question 26
+            //Define a function to find the avarage
+            //of a student that has 7 grades and print the
+            //result in the void main\n\n");
+
+            int[] arrayAvarage = {1,2,30,4,5,6,7 };
+
+            double avarage = AvarageRate(arrayAvarage);
+            Console.WriteLine($" The the avarage of a student that has 7 grades is {avarage}\n\n"); 
+
+        }// Close ==> static void Main(string[] args)
+    }
+}
