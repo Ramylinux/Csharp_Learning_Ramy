2d74bba242b1024910179f270733b726ca90cefe
Ramylinux <ramy_work@yahoo.com>
2017-11-12 14:19:42 +0300

    Tutorial_29_Main_Method

---
+++ Tutorial_29_Main_Method/Tutorial_29_Main_Method/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_29_Main_Method
+{
+    class Program
+    {
+        static int Main(string[] args) // You can Change static string Main(string[] args) To static int Main()
+                                       // OR 
+                                       // static string Main()
+        {
+            Console.WriteLine($"{args.Length} Command Line Arguments has Entered: ");
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+
+            }
+            Console.ReadKey();
+            return 0; // Need return because change static string Main(string[] args) to static int Main(string[] args)
+
+        }//Colse ==> Main Function
+        
+    }
+}
