9eac31fd140554360cfa0bc212681f51ec6c45f9
Ramylinux <ramy_work@yahoo.com>
2017-11-12 16:18:13 +0300

    Tutorial_31_Overloading

---
+++ Tutorial_31_Overloading/Tutorial_31_Overloading/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_31_Overloading
+{
+    class Program
+    {
+        public static int printNumber(int myNumber)
+        {
+
+            return myNumber;
+        }
+        public static double printNumber(double myNumber)
+        {
+            return myNumber;
+        }
+        static void Main(string[] args)
+        {
+            int myNumber = 9;
+            Console.Write($"{printNumber(myNumber)}");
+            Console.ReadKey();
+
+            //Q29
+            //Define two functions using overloading concepts to return the minimum value of an 
+            //array with 10 numbers once as integer and once as double value and print the result
+
+        }//Colse==> static void Main(string[] args)
+
+    }
+}
