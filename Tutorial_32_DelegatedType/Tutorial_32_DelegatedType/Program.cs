8fc093ba8fdb27f2dd0239b02c91d04d3ba97aee
Ramylinux <ramy_work@yahoo.com>
2017-11-15 14:47:28 +0300

    Tutorial_32_DelegatedType

---
+++ Tutorial_32_DelegatedType/Tutorial_32_DelegatedType/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_32_DelegatedType
+{
+    delegate int MyNewNumber(int myNumber);
+    class Program
+    {
+        static int myXNumber = 3;
+        
+        static int addFunct(int number)
+        {
+            myXNumber += number;
+            return myXNumber;
+        }
+        static int mulFunc(int number)
+        {
+            myXNumber *= number;
+            return myXNumber;
+        }
+        static void Main(string[] args)
+        {
+
+            MyNewNumber n1 = new MyNewNumber(addFunct);
+            MyNewNumber n2 = new MyNewNumber(mulFunc);
+
+            n1(4);
+            Console.WriteLine($"{myXNumber}\n");
+            n2(5);
+            Console.WriteLine($"{myXNumber}");
+
+            Console.ReadKey();
+
+        }//Colse==>static void Main(string[] args)
+         
+    }
+}
