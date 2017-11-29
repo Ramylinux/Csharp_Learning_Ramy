473449d26b54433e4732e18305a53bb22f63d62c
Ramylinux <ramy_work@yahoo.com>
2017-11-29 15:39:41 +0300

    null

---
+++ Tutorial_41_Null_Able_Values/Tutorial_41_Null_Able_Values/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Tutorial_41_Null_Able_Values
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myVariable=null;
+            int? myNumber = null;
+            Write($"This isMy String is null = {myVariable}\n");
+            Write($"This is  My Number is null = {myNumber}\n\n");
+            ReadKey();
+
+
+        }//Colse Main Function ==>  static void Main(string[] args)
+
+    }
+}
