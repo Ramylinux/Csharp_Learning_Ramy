375c118a2d5ea646660fa8ebb963afa44358c608
Ramylinux <ramy_work@yahoo.com>
2017-12-10 15:20:47 +0300

    Recursive abd factory 5

---
+++ Tutorial_45_GoTo/Tutorial_45_GoTo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Tutorial_45_GoTo
+{
+    class Program
+    {
+        static int MyFunction()
+        {
+            int counter = 0;
+            for (int i=0;i<10;i++)
+            {
+                for (int j=0;j<10;j++)// Untel Condition Execute
+                {
+                    if (j==5)
+                    {
+                        goto MyLabel1;
+                    }
+                    counter++;
+                }
+            MyLabel1:
+                continue;
+            }
+            return counter;
+        }//Close ==> static int MyFunction()
+        static void Main(string[] args)
+        {
+            WriteLine(MyFunction());
+            ReadKey();
+
+        }//Close ==>  static void Main(string[] args)
+
+    }
+}
---
+++ Tutorial_46_Recursive_Function/Tutorial_46_Recursive_Function/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_46_Recursive_Function
+{
+    class Program
+    {
+        static int MyRecursiveFunction(int myValue, ref int counter)
+        {
+            counter++;
+            //if value = 0
+            if (myValue == 0)
+                return 0;
+            //if value less than 10
+            else if (myValue < 10)
+            {
+                return MyRecursiveFunction(myValue + 1, ref counter);
+
+            }
+            //if value equal or exceeds 10
+            return myValue;
+        }
+        //--------------------------------------------------
+        //Challenge 32 
+        static int FactorialCalcu(int n1)
+        {
+            if (n1 == 0)
+            {
+                return 1;
+            }
+            return n1 * FactorialCalcu(n1 - 1);
+        }
+        //--------------------------------------------------
+        static void Main(string[] args)
+        {
+            int counter = 0, myResult;
+            myResult = MyRecursiveFunction(4, ref counter);
+            Console.WriteLine($"My Result is {myResult} and Counter value is {counter}");
+            Console.ReadKey();
+
+
+            //Challenge 32 
+            // Find Factorial the number n where n =5;
+
+            int fact = FactorialCalcu(5);
+            Console.WriteLine($" The factorial of 5 is : {fact}\n");
+
+
+        }//Colse ==> Main Function ==>static void Main(string[] args)
+
+    }
+}
