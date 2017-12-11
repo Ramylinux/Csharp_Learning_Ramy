15d7bdf9f903d0a6c23aec741ef2fe34ab940b72
Ramylinux <ramy_work@yahoo.com>
2017-12-11 15:34:54 +0300

    Tutorial_48_Palindrome_Function

---
+++ Tutorial_48_Palindrome_Function/Tutorial_48_Palindrome_Function/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_48_Palindrome_Function
+{
+    class Program
+    {
+        public static bool MyPalindromeFunction(string value)
+        {
+            int min = 0;
+            int max = value.Length - 1;
+            while (true)
+            {
+                if (min > max)
+                {
+                    return true;
+                }
+                char a = value[min];
+                char b = value[max];
+                if (char.ToLower(a) != char.ToLower(b))
+                {
+                    return false;
+                }
+                min++;
+                max--;
+            }
+        }
+
+        public static void Main()
+        {
+            string[] myArray = { "civic", "deified", "deleveled", "Perls", "Is", "Not", "A", "Palindrome", "" };
+
+            foreach (string word in myArray)
+            {
+                Console.WriteLine("{0} = {1}", word, MyPalindromeFunction(word));
+            }
+            Console.ReadKey();
+
+        }//Close==> Main Function ==>  static void Main(string[] args)
+
+    }
+}
