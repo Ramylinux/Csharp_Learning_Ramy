129fd904ef02f4be445663bf3eae652de87c121c
Ramylinux <ramy_work@yahoo.com>
2017-12-11 15:14:41 +0300

    Tutorial_47_Optional_Parameters

---
+++ ConsoleApplication-testing/ConsoleApplication-testing/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace ConsoleApplication_testing
+{
+    class Program
+    {
+
+        private Dictionary<string , string> errors = new Dictionary<string, string>();
+        private List<string> result = new List<string>();
+        private void SearchForFiles(string path)
+        {
+            try
+            {
+                foreach (string fileName in Directory.GetFiles(path))//Gets all files in the current path
+                {
+                    result.Add(fileName);
+                }
+
+                foreach (string directory in Directory.GetDirectories(path))//Gets all folders in the current path
+                {
+                    SearchForFiles(directory);//The methods calls itself with a new parameter, here!
+                }
+            }
+            catch (System.Exception ex)
+            {
+                errors.Add(path, ex.Message);//Stores Error Messages in a dictionary with path in key
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+           
+        }//
+    }
+}
---
+++ Tutorial_47_Optional_Parameters/Tutorial_47_Optional_Parameters/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_47_Optional_Parameters
+{
+    class Program
+    {
+        public static void PrintValues(string name = "Ramy AbuDhair", int myValue = 7)
+        {
+            Console.WriteLine($"Name  = {name}  Number  = {myValue}");
+
+        }
+
+        static void Main(string[] args)
+        {
+            string name = "Ahmed";
+            int value = 15;
+            PrintValues();
+            PrintValues(name);
+            PrintValues(name, value);
+            PrintValues(name: "Hammad", myValue: 6);
+            Console.ReadKey();
+           
+
+        }//Close ==> Main Function ==> static void Main(string[] args)
+
+    }
+}
