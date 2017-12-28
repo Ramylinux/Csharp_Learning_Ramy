60b36ced7de3fd9ca288ae22fcd098d6d10c147e
Ramylinux <ramy_work@yahoo.com>
2017-12-28 13:56:28 +0300

    static data

---
+++ OOP-Coding/Tutorial_51_StaticData_Methods/Tutorial_51_StaticData_Methods/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_51_StaticData_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StaticData_Methods s1 = new StaticData_Methods(50); //Add the Value To public double currBalance
+            StaticData_Methods s2 = new StaticData_Methods(100); //Add the Value To public double currBalance
+            //---------------------------------------------------------------
+            Console.WriteLine("CurrBalance S1 is: {0}",s1.currBalance );
+            Console.WriteLine("CurrBalance S2 is: {0}", s2.currBalance);
+            //---------------------------------------------------------------
+            Console.WriteLine("\n");
+
+            // Print the current interest rate.
+            Console.WriteLine("Interest Rate is static constructor : {0}", StaticData_Methods.GetValue()); //Call Class StaticData_Methods
+
+           StaticData_Methods.SetInterestRate(0.7);
+
+            //Even If we make new object, this does NOT ’reset’ the interest rate.
+            StaticData_Methods s3 = new StaticData_Methods(2000);
+
+            //---------------------------------------------------------------
+            Console.WriteLine("CurrBalance S3 is: {0}", s3.currBalance);
+            Console.WriteLine("\n");
+            //---------------------------------------------------------------
+
+            Console.WriteLine("Interest Rate is: {0}", StaticData_Methods.GetValue());
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ OOP-Coding/Tutorial_51_StaticData_Methods/Tutorial_51_StaticData_Methods/StaticData_Methods.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_51_StaticData_Methods
+{
+    class StaticData_Methods
+    {
+        // Static Data at Instance-level.
+        public double currBalance; // Call by Object for this class
+
+        // A static point of data.
+        //public static double currInterestRate;
+        public static double currInterestRate; //Call by Class
+
+        //Parameterized Constructor
+        public StaticData_Methods(double balance)
+        {
+            currBalance = balance;
+         //   currInterestRate = 0.5;
+            
+        }
+
+        //Static constructor 
+        static StaticData_Methods() //none parametars constructor  
+        {
+            currInterestRate = 0.5;  //this Value used one time when create instance objects
+        } // After this static constructor the funcetion Getvalue  read value = 0.7 SO, if this function not avalabile, The Getvalue function  read value = 0.5
+
+
+        public static void SetInterestRate(double newRate)
+        { currInterestRate = newRate; }
+
+        public static double GetValue()
+        { return currInterestRate; }
+
+       
+
+    }
+}
