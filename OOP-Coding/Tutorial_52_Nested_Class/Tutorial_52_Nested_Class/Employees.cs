fa34ca53913e45527c978bdaed1db13ad91de023
Ramylinux <ramy_work@yahoo.com>
2018-01-01 13:39:32 +0300

    Nestest Class

---
+++ OOP-Coding/Tutorial_52_Nested_Class/Tutorial_52_Nested_Class/Employees.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_52_Nested_Class
+{
+   class Employees //Class 1
+   {
+      public string fName = "Ramy Abu-Dhair";
+      public string Address = "Jordan - Amman";
+      public void PrintValues() { Console.WriteLine($"Employee Name is  {fName} \n Living at {Address}"); }
+
+      public class Departments //Class 2
+      {
+         public string departmentName = "I.T Administration";
+         public void PrintDepartmentName() { Console.WriteLine($"Working at  {departmentName}"); }
+
+      }
+
+   }
+
+
+
+}
---
+++ OOP-Coding/Tutorial_52_Nested_Class/Tutorial_52_Nested_Class/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_52_Nested_Class
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Employees myEmployee = new Employees(); // Access to Class 1 but you cann't access to class 2
+         myEmployee.PrintValues();
+
+         Employees.Departments myDepart = new Employees.Departments(); // Access to Class 2
+         myDepart.PrintDepartmentName();
+         Console.WriteLine();
+         Console.ReadKey();
+
+      }
+
+
+
+
+   }
+
+ }
