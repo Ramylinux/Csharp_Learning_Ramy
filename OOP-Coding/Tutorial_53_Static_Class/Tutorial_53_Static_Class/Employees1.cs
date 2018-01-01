fa34f29dee69fa5f554e26037142135b4ddcdd60
Ramylinux <ramy_work@yahoo.com>
2018-01-01 13:49:37 +0300

    utorial_53_Static_Class

---
+++ OOP-Coding/Tutorial_53_Static_Class/Tutorial_53_Static_Class/Employees1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_53_Static_Class
+{
+   static class Employees1 //add static to class
+   {
+      static public string name = "Ramy Abu-Dhair\n"; //add static to Variables ( static data)
+      static public int age = 39; //add static to Variables ( static data)
+
+      static public void PrintName() //add static to Methods ( static Methods) // and add (void) for no return any value
+      {
+         Console.WriteLine($"{"Name = " + name}{"AGe = " + age}");
+      }
+   }
+
+
+
+}
---
+++ OOP-Coding/Tutorial_53_Static_Class/Tutorial_53_Static_Class/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_53_Static_Class
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         //No Need Create instance Objects for class
+         // same as : Employees1 myName = new Employees1; // this is Wrong because this class is (static class )
+
+
+         Employees1.PrintName(); // Call (static class) direct
+         Console.ReadKey();
+      }
+
+
+
+
+   }
+}
