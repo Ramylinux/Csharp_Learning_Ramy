fb10b986257f2ee7363a01c9acdc24b5249eed8a
Ramylinux <ramy_work@yahoo.com>
2018-01-04 14:37:20 +0300

    Partial Methods

--- OOP-Coding/Tutorial_54_Paratial_Classes/Tutorial_54_Paratial_Classes/EmployeesPart1.cs
+++ OOP-Coding/Tutorial_54_Paratial_Classes/Tutorial_54_Paratial_Classes/EmployeesPart1.cs
@@ -6,12 +6,14 @@ using System.Threading.Tasks;
 
 namespace Tutorial_54_Paratial_Classes
 {
-   partial class EmployeesPart
+   partial class EmployeesPart //partial Class 1 EmployeesPart1.cs
    {
       public string name = "Ramy Abu-Dhair";
       public string address = "Amman-Jordan";
       public string department = "I.T Departments";
-
+      public int age = 39;
+      //Partial Methods
+      partial void MyAge(); // Create Partial Methods
       public EmployeesPart()
        { }
 
--- OOP-Coding/Tutorial_54_Paratial_Classes/Tutorial_54_Paratial_Classes/EmployeesPart2.cs
+++ OOP-Coding/Tutorial_54_Paratial_Classes/Tutorial_54_Paratial_Classes/EmployeesPart2.cs
@@ -6,11 +6,22 @@ using System.Threading.Tasks;
 
 namespace Tutorial_54_Paratial_Classes
 {
-   partial class EmployeesPart
+   partial class EmployeesPart //partial Class 2 EmployeesPart2.cs
    {
       public void PrintName() { Console.WriteLine($"Name  = {name}"); }
       public void PrintAdress() { Console.WriteLine($"Adress  = {address}"); }
-      public void PrintDepartment() { Console.WriteLine($"Department  = {department}"); }
+      //1: implantation for partial Methods 
+      partial void MyAge() // implantation for partial Methods
+      {
+         Console.WriteLine($"Employee Age is = {age}");
+      }
+      //2: Call partial Method by Function  public void PrintDepartment()
+      public void PrintDepartment()
+      {
+         Console.WriteLine($"Department  = {department}");
+         MyAge(); // Call partial Method by Function  public void PrintDepartment()
+      }
+
 
    }
 }
