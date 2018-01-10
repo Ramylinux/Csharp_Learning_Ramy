0fc7e986580552270416f6c29f8d636b6f71f1b1
Ramylinux <ramy_work@yahoo.com>
2018-01-10 10:35:50 +0300

    Tutorial_55_EnacapsulationApp

---
+++ OOP-Coding/Tutorial_55_EnacapsulationApp/Tutorial_55_EnacapsulationApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_55_EnacapsulationApp
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         string name = "Ramy Abu-Dhair";
+         int age = 39;
+         Person myObj = new Person();
+         myObj.SetName(name); // Send value to Private variables
+         myObj.SetAge(age); // Send value to Private variables
+
+         Console.WriteLine("Name is " + myObj.GetName()); // Get Value from Private Variables
+         Console.WriteLine("Age is " + myObj.GetAge()); // Get Value from Private Variables
+
+         Console.ReadKey();
+      }
+   }
+}
